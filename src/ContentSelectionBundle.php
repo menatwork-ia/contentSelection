3db92b5a750418f982705e6896ac47f13bb6868a
Andreas Dziemba <dziemba@men-at-work.de>
2018-03-14 17:44:56 +0100

    Add Missing Bundle File

---
+++ src/ContentSelectionBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2018
+ * @package    MenAtWork\ContentSelectionBundle
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+namespace MenAtWork\ContentSelectionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ContentSelectionBundle
+ *
+ * @package ManAtWork\ContentSelectionBundle
+ */
+class ContentSelectionBundle extends Bundle
+{
+}
